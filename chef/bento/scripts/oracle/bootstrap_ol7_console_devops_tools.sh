66a49abdb233ddde5f9b845e016dc2e8a439b891
Ed Barberis <ed.barberis@gmail.com>
2017-01-31 18:59:24 -0500

    Added support for Python 2.x, JDK 8u121, and Ant contrib.

--- chef/bento/scripts/oracle/bootstrap_ol7_console_devops_tools.sh
+++ chef/bento/scripts/oracle/bootstrap_ol7_console_devops_tools.sh
@@ -1,8 +1,34 @@
 #!/bin/sh -eux
 # install useful command-line developer tools.
 
+# create temporary scripts directory. ------------------------------------------
+mkdir -p /tmp/scripts/oracle
+cd /tmp/scripts/oracle
+
+# install epel repository if needed. -------------------------------------------
+if [ ! -f "/etc/yum.repos.d/epel.repo" ]; then
+  wget --no-verbose https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+  yum repolist
+  yum -y install epel-release-latest-7.noarch.rpm
+fi
+
 # update the repository list. --------------------------------------------------
 yum repolist
+
+# install python 2.x pip and setuptools. ---------------------------------------
+yum -y install python-pip
+python --version
+pip --version
+
+# upgrade python 2.x pip.
+pip install --upgrade pip
+pip --version
+
+# install python 2.x setup tools.
+yum -y install python-setuptools
+#pip install --upgrade setuptools
+pip install 'setuptools==33.1.1'
+easy_install --version
 
 # install software collections library. (needed later for python 3.x.) ---------
 yum -y install scl-utils
@@ -11,50 +37,22 @@ yum -y install scl-utils
 yum -y install git
 git --version
 
-# install git flow. ------------------------------------------------------------
-# download and install epel repository if needed.
-epel_repo="/etc/yum.repos.d/epel.repo"
-if [ ! -f "$epel_repo" ]; then
-  # create temporary scripts directory.
-  mkdir -p /tmp/scripts/oracle
-  cd /tmp/scripts/oracle
-
-  # install the epel repository.
-  wget --no-verbose https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-  yum repolist
-  yum -y install epel-release-latest-7.noarch.rpm
-fi
-
-# install git flow.
-yum -y install gitflow
-
-# verify git flow installation.
-#git flow version
-
-# install git completion for bash. ---------------------------------------------
-gcbin=".git-completion.bash"
-gcfolder="/home/vagrant"
-
-# download git completion for bash from github.com.
-curl --silent --location "https://github.com/git/git/raw/master/contrib/completion/git-completion.bash" --output ${gcfolder}/${gcbin}
-chown -R vagrant:vagrant ${gcfolder}/${gcbin}
-chmod 644 ${gcfolder}/${gcbin}
-
-# install oracle java se development kit 8u111. --------------------------------
-jdkbuild="8u111-b14"
-jdkbinary="jdk-8u111-linux-x64.tar.gz"
-jdkfolder="jdk1.8.0_111"
+# install oracle java se development kit. --------------------------------------
+jdkbuild="8u121-b13"
+jdkhash="e9e7ea248e2c4826b92b3f075a80e441"
+jdkbinary="jdk-8u121-linux-x64.tar.gz"
+jdkfolder="jdk1.8.0_121"
 
 # create java home parent folder.
 mkdir -p /usr/local/java
 cd /usr/local/java
 
 # download jdk binary from oracle otn.
-wget --no-verbose --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/${jdkbuild}/${jdkbinary}
+wget --no-verbose --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/${jdkbuild}/${jdkhash}/${jdkbinary}
 #wget --no-verbose --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip
 
 # extract jdk binary and create softlink to 'jdk180'.
-tar -zxvf ${jdkbinary}
+tar -zxvf ${jdkbinary} --no-same-owner --no-overwrite-dir
 chown -R root:root ./${jdkfolder}
 ln -s ${jdkfolder} jdk180
 rm -f ${jdkbinary}
@@ -68,11 +66,9 @@ export PATH
 # verify installation.
 java -version
 
-# install apache ant 1.10.0. ---------------------------------------------------
-antbinary="apache-ant-1.10.0-bin.tar.gz"
+# install apache ant. ----------------------------------------------------------
 antfolder="apache-ant-1.10.0"
-#antbinary="apache-ant-1.9.7-bin.tar.gz"
-#antfolder="apache-ant-1.9.7"
+antbinary="${antfolder}-bin.tar.gz"
 
 # create apache parent folder.
 mkdir -p /usr/local/apache
@@ -82,12 +78,13 @@ cd /usr/local/apache
 wget --no-verbose http://archive.apache.org/dist/ant/binaries/${antbinary}
 
 # extract ant binary.
-tar -zxvf ${antbinary}
+tar -zxvf ${antbinary} --no-same-owner --no-overwrite-dir
 chown -R root:root ./${antfolder}
+ln -s ${antfolder} apache-ant
 rm -f ${antbinary}
 
 # set ant home environment variables.
-ANT_HOME=/usr/local/apache/${antfolder}
+ANT_HOME=/usr/local/apache/apache-ant
 export ANT_HOME
 PATH=${ANT_HOME}/bin:$PATH
 export PATH
@@ -95,13 +92,32 @@ export PATH
 # verify installation.
 ant -version
 
-# install apache maven 3.3.9. --------------------------------------------------
+# install apache ant contrib. --------------------------------------------------
+acfolder="ant-contrib"
+acrelease="1.0b3"
+acbinary="${acfolder}-${acrelease}-bin.tar.gz"
+acjar="${acfolder}-${acrelease}.jar"
+
+# create apache contrib parent folder.
+mkdir -p /usr/local/apache
+cd /usr/local/apache
+
+# download ant contrib library from sourceforge.net.
+curl --silent --location "https://sourceforge.net/projects/${acfolder}/files/${acfolder}/${acrelease}/${acbinary}/download" --output ${acbinary}
+
+# extract ant contrib binary.
+tar -zxvf ${acbinary} --no-same-owner --no-overwrite-dir
+chown -R root:root ./${acfolder}
+rm -f ${acbinary}
+
+# copy ant contrib library to apache-ant/lib.
+cd /usr/local/apache/${acfolder}
+cp -p ${acjar} /usr/local/apache/apache-ant/lib
+
+# install apache maven. --------------------------------------------------------
 mvnrelease="3.3.9"
-mvnbinary="apache-maven-3.3.9-bin.tar.gz"
-mvnfolder="apache-maven-3.3.9"
-#mvnrelease="3.2.5"
-#mvnbinary="apache-maven-3.2.5-bin.tar.gz"
-#mvnfolder="apache-maven-3.2.5"
+mvnfolder="apache-maven-${mvnrelease}"
+mvnbinary="${mvnfolder}-bin.tar.gz"
 
 # create apache parent folder.
 mkdir -p /usr/local/apache
@@ -111,12 +127,13 @@ cd /usr/local/apache
 wget --no-verbose http://archive.apache.org/dist/maven/maven-3/${mvnrelease}/binaries/${mvnbinary}
 
 # extract maven binary.
-tar -zxvf ${mvnbinary}
+tar -zxvf ${mvnbinary} --no-same-owner --no-overwrite-dir
 chown -R root:root ./${mvnfolder}
+ln -s ${mvnfolder} apache-maven
 rm -f ${mvnbinary}
 
 # set maven home environment variables.
-M2_HOME=/usr/local/apache/${mvnfolder}
+M2_HOME=/usr/local/apache/apache-maven
 export M2_HOME
 M2_REPO=$HOME/.m2
 export M2_REPO
