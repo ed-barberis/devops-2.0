d8f1b30a45582a2476614dce1a3f38e01e1a02e2
Ed Barberis <ed.barberis@gmail.com>
2017-01-31 19:00:53 -0500

    Added support for installing ansible container.

--- chef/bento/scripts/oracle/bootstrap_ol7_ansible.sh
+++ chef/bento/scripts/oracle/bootstrap_ol7_ansible.sh
@@ -1,14 +1,12 @@
 #!/bin/sh -eux
 # install ansible on oracle linux 7.x.
 
-# install epel repository if needed. -------------------------------------------
-epel_repo="/etc/yum.repos.d/epel.repo"
-if [ ! -f "$epel_repo" ]; then
-  # create temporary scripts directory.
-  mkdir -p /tmp/scripts/oracle
-  cd /tmp/scripts/oracle
+# create temporary scripts directory. ------------------------------------------
+mkdir -p /tmp/scripts/oracle
+cd /tmp/scripts/oracle
 
-  # install the epel repository.
+# install epel repository if needed. -------------------------------------------
+if [ ! -f "/etc/yum.repos.d/epel.repo" ]; then
   wget --no-verbose https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
   yum repolist
   yum -y install epel-release-latest-7.noarch.rpm
@@ -17,12 +15,34 @@ fi
 # install ansible. -------------------------------------------------------------
 yum -y install ansible
 
-# verify ansible installation. -------------------------------------------------
+# verify ansible installation.
 ansible --version
 
 # upgrade ansible installation. ------------------------------------------------
 yum-config-manager --enable epel-testing
 yum -y upgrade ansible
 
-# verify ansible upgrade installation. -----------------------------------------
+# verify ansible upgrade installation.
 ansible --version
+
+# install ansible container. ---------------------------------------------------
+# install latest from repository.
+#pip install ansible-container
+
+# verify installation (0.2.0).
+#ansible-container version
+
+# build and install latest ansible container binaries from source. -------------
+# create ansible container source parent folder.
+mkdir -p /usr/local/src/ansible
+cd /usr/local/src/ansible
+
+# download ansible container source from github.com.
+git clone https://github.com/ansible/ansible-container.git ansible-container
+
+# build and install ansible container binaries.
+cd ansible-container
+python ./setup.py install
+
+# verify installation (0.3.0-pre).
+ansible-container version
