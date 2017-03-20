94c9cca1c9e910f85ec973f7b867fc2c7bc4104d
Ed Barberis <ed.barberis@gmail.com>
2017-03-20 17:09:27 -0400

    Added support for consul, vault, and terraform.

---
+++ shared/scripts/oracle/bootstrap_ol7_hashicorp_consul.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -eux
+# install consul service discovery and configuration tool by hashicorp.
+
+# install hashicorp consul. ----------------------------------------------------
+consul_release="0.7.5"
+consul_binary="consul_${consul_release}_linux_amd64.zip"
+
+# create local bin directory (if needed).
+mkdir -p /usr/local/bin
+cd /usr/local/bin
+
+# download consul binary from hashicorp.com.
+wget --no-verbose https://releases.hashicorp.com/consul/${consul_release}/${consul_binary}
+
+# extract consul binary.
+unzip ${consul_binary}
+chmod 755 consul
+rm -f ${consul_binary}
+
+# set consul environment variables.
+#PATH=/usr/local/bin:$PATH
+#export PATH
+
+# verify installation.
+#consul --version
---
+++ shared/scripts/oracle/bootstrap_ol7_hashicorp_vault.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -eux
+# install vault tool for managing secrets by hashicorp.
+
+# install hashicorp vault. -----------------------------------------------------
+vault_release="0.7.0"
+vault_binary="vault_${vault_release}_linux_amd64.zip"
+
+# create local bin directory (if needed).
+mkdir -p /usr/local/bin
+cd /usr/local/bin
+
+# download vault binary from hashicorp.com.
+wget --no-verbose https://releases.hashicorp.com/vault/${vault_release}/${vault_binary}
+
+# extract vault binary.
+unzip ${vault_binary}
+chmod 755 vault
+rm -f ${vault_binary}
+
+# set vault environment variables.
+#PATH=/usr/local/bin:$PATH
+#export PATH
+
+# verify installation.
+#vault --version
