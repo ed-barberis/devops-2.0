285a8fed49565ed412730d4255870a05034ba47e
Ed Barberis <ed.barberis@gmail.com>
2017-06-09 22:58:27 -0400

    Added support for US and EMEA data centers.

--- projects/generic/vms/ops/scripts/bootstrap_ol7_oracle_psm_cli.sh
+++ projects/generic/vms/ops/scripts/bootstrap_ol7_oracle_psm_cli.sh
@@ -17,17 +17,21 @@ scl enable python33 -- pip3 --version
 username="<username>"
 password="<password>"
 identity_domain="<identity_domain>"
+data_center="<valid values are 'us' and 'europe'>"
+region="<valid values are 'us', 'emea', and 'aucom'>"
+format="<valid values are 'short', 'json', and 'html'>"
 
 # retrieve and install the psm-cli from the oracle cloud.
-curl --silent --request GET --user ${username}:${password} --header X-ID-TENANT-NAME:${identity_domain} https://psm.europe.oraclecloud.com/paas/core/api/v1.1/cli/${identity_domain}/client --output psmcli.zip
+curl --silent --request GET --user ${username}:${password} --header X-ID-TENANT-NAME:${identity_domain} https://psm.${data_center}.oraclecloud.com/paas/core/api/v1.1/cli/${identity_domain}/client --output psmcli.zip
 scl enable python33 -- pip3 install --upgrade psmcli.zip
 runuser -c "scl enable python33 -- psm --version" - vagrant
 
 # configure the psm-cli client. ------------------------------------------------
-psm_setup_cmd=$(printf "psm setup <<< \$\'%s\\\\n%s\\\\n%s\\\\n%s\\\\n%s\\\\n%s\\\\n\'\n" ${username} ${password} ${password} ${identity_domain} emea json)
+psm_setup_cmd=$(printf "psm setup <<< \$\'%s\\\\n%s\\\\n%s\\\\n%s\\\\n%s\\\\n%s\\\\n\'\n" ${username} ${password} ${password} ${identity_domain} ${region} ${format})
 runuser -c "scl enable python33 -- eval ${psm_setup_cmd}" - vagrant
 
 # verify the psm-cli configuration. --------------------------------------------
+runuser -c "scl enable python33 -- psm accs apps" - vagrant
 runuser -c "scl enable python33 -- psm dbcs services" - vagrant
 runuser -c "scl enable python33 -- psm jcs services" - vagrant
 
