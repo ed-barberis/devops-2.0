7779d1a80b5eb6be220f61898d914b0f96bc69f6
Ed Barberis <ed.barberis@gmail.com>
2018-01-25 12:17:12 -0500

    Upgraded Git, Go, and Consul to latest versions.

--- provisioners/scripts/common/install_hashicorp_consul.sh
+++ provisioners/scripts/common/install_hashicorp_consul.sh
@@ -2,7 +2,7 @@
 # install consul service discovery and configuration tool by hashicorp.
 
 # install hashicorp consul. ----------------------------------------------------
-consul_release="1.0.2"
+consul_release="1.0.3"
 consul_binary="consul_${consul_release}_linux_amd64.zip"
 
 # create local bin directory (if needed).
