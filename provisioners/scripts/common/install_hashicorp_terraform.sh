97376dff260dbbae0a0d45a6c9a018494dfc60a1
Ed Barberis <ed.barberis@gmail.com>
2018-02-10 23:25:08 -0500

    Upgraded Hashicorp utilities to latest versions.

--- provisioners/scripts/common/install_hashicorp_terraform.sh
+++ provisioners/scripts/common/install_hashicorp_terraform.sh
@@ -2,7 +2,7 @@
 # install terraform infrastructure as code tool by hashicorp.
 
 # install hashicorp terraform. -------------------------------------------------
-terraform_release="0.11.2"
+terraform_release="0.11.3"
 terraform_binary="terraform_${terraform_release}_linux_amd64.zip"
 
 # create local bin directory (if needed).
