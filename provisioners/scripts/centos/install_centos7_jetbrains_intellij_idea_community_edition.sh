d7f4fbbf8ad4604974cbab292b5ea15904691e90
Ed Barberis <ed.barberis@gmail.com>
2018-03-30 15:47:51 -0400

    Added support for JetBrains IntelliJ IDEA and WebStorm versions 2018.1.

---
+++ provisioners/scripts/centos/install_centos7_jetbrains_intellij_idea_community_edition.sh
@@ -0,0 +1,54 @@
+#!/bin/sh -eux
+# install intellij idea community edition by jetbrains.
+
+# set default value for devops home environment variable if not set. -----------
+devops_home="${devops_home:-/opt/devops}"                   # [optional] devops home (defaults to '/opt/devops').
+
+# install intellij idea community edition. -------------------------------------
+idea_home="idea"
+idea_release="2018.1"
+idea_build="181.4203.550"
+
+idea_binary="ideaIC-${idea_release}-no-jdk.tar.gz"
+idea_folder="idea-IC-${idea_build}"
+
+# create jetbrains home parent folder.
+mkdir -p /usr/local/jetbrains
+cd /usr/local/jetbrains
+
+# download intellij idea community edition binary.
+wget --no-verbose https://download.jetbrains.com/idea/${idea_binary}
+
+# extract intellij idea community edition binary.
+rm -f ${idea_home}
+tar -zxvf ${idea_binary} --no-same-owner --no-overwrite-dir
+chown -R root:root ./${idea_folder}
+ln -s ${idea_folder} ${idea_home}
+rm -f ${idea_binary}
+
+# install intellij idea community edition as gnome desktop app. ----------------
+imgname="intellij-idea-logo"
+imgsizearray=( "16x16" "22x22" "24x24" "32x32" "48x48" "64x64" "128x128" "256x256" )
+imgfolder="/usr/share/icons/hicolor"
+
+cd ${devops_home}/provisioners/scripts/centos
+
+# install intellij idea community edition icon image files.
+for imgsize in "${imgsizearray[@]}"; do
+  if [ -d "${imgfolder}/${imgsize}/apps" ]; then
+    echo "Processing ${imgname}-${imgsize}.png..."
+    install -o root -g root -m 0644 ./images/${imgname}-${imgsize}.png ${imgfolder}/${imgsize}/apps/${imgname}.png
+  fi
+done
+
+# install intellij idea community edition desktop in applications menu. --------
+echo "Installing intellij-idea.desktop in applications menu..."
+desktop-file-install --dir=/usr/share/applications/ ./desktops/intellij-idea.desktop
+update-desktop-database /usr/share/applications/
+
+# copy intellij idea community edition launcher to devops applications folder. -
+echo "Copying intellij-idea.desktop launcher to devops 'applications' folder..."
+mkdir -p ${devops_home}/provisioners/scripts/centos/applications
+cd ${devops_home}/provisioners/scripts/centos/applications
+cp -f /usr/share/applications/intellij-idea.desktop .
+chmod 755 ./intellij-idea.desktop
---
+++ provisioners/scripts/centos/install_centos7_jetbrains_webstorm_javascript_ide.sh
@@ -0,0 +1,54 @@
+#!/bin/sh -eux
+# install webstorm javascript ide by jetbrains.
+
+# set default value for devops home environment variable if not set. -----------
+devops_home="${devops_home:-/opt/devops}"                   # [optional] devops home (defaults to '/opt/devops').
+
+# install webstorm javascript ide. ---------------------------------------------
+webstorm_home="webstorm"
+webstorm_release="2018.1"
+webstorm_build="181.4203.535"
+
+webstorm_binary="WebStorm-${webstorm_release}.tar.gz"
+webstorm_folder="WebStorm-${webstorm_build}"
+
+# create jetbrains home parent folder.
+mkdir -p /usr/local/jetbrains
+cd /usr/local/jetbrains
+
+# download webstorm javascript ide binary.
+wget --no-verbose https://download.jetbrains.com/webstorm/${webstorm_binary}
+
+# extract webstorm javascript ide binary.
+rm -f ${webstorm_home}
+tar -zxvf ${webstorm_binary} --no-same-owner --no-overwrite-dir
+chown -R root:root ./${webstorm_folder}
+ln -s ${webstorm_folder} ${webstorm_home}
+rm -f ${webstorm_binary}
+
+# install webstorm javascript ide as gnome desktop app. ------------------------
+imgname="webstorm-logo"
+imgsizearray=( "16x16" "22x22" "24x24" "32x32" "48x48" "64x64" "128x128" "256x256" )
+imgfolder="/usr/share/icons/hicolor"
+
+cd ${devops_home}/provisioners/scripts/centos
+
+# install webstorm javascript ide icon image files.
+for imgsize in "${imgsizearray[@]}"; do
+  if [ -d "${imgfolder}/${imgsize}/apps" ]; then
+    echo "Processing ${imgname}-${imgsize}.png..."
+    install -o root -g root -m 0644 ./images/${imgname}-${imgsize}.png ${imgfolder}/${imgsize}/apps/${imgname}.png
+  fi
+done
+
+# install webstorm javascript ide desktop in applications menu. ----------------
+echo "Installing webstorm.desktop in applications menu..."
+desktop-file-install --dir=/usr/share/applications/ ./desktops/webstorm.desktop
+update-desktop-database /usr/share/applications/
+
+# copy webstorm javascript ide launcher to devops applications folder. ---------
+echo "Copying webstorm.desktop launcher to devops 'applications' folder..."
+mkdir -p ${devops_home}/provisioners/scripts/centos/applications
+cd ${devops_home}/provisioners/scripts/centos/applications
+cp -f /usr/share/applications/webstorm.desktop .
+chmod 755 ./webstorm.desktop
