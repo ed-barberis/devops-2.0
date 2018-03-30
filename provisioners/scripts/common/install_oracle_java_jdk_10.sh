1411537c018369be400bce7bf13e14b3c05737b2
Ed Barberis <ed.barberis@gmail.com>
2018-03-30 15:33:14 -0400

    Added support for Java SE JDK 10.

---
+++ provisioners/scripts/common/install_oracle_java_jdk_10.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -eux
+# install java se 10 development kit by oracle.
+
+# install java se 10 development kit. ------------------------------------------
+jdkhome="jdk10"
+jdkbuild="10+46"
+jdkhash="76eac37278c24557a3c4199677f19b62"
+jdkfolder="jdk-10"
+jdkbinary="${jdkfolder}_linux-x64_bin.tar.gz"
+
+# create java home parent folder.
+mkdir -p /usr/local/java
+cd /usr/local/java
+
+# download jdk 10 binary from oracle otn.
+wget --no-verbose --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/${jdkbuild}/${jdkhash}/${jdkbinary}
+
+# extract jdk 10 binary and create softlink to 'jdk10'.
+rm -f ${jdkhome}
+tar -zxvf ${jdkbinary} --no-same-owner --no-overwrite-dir
+chown -R root:root ./${jdkfolder}
+ln -s ${jdkfolder} ${jdkhome}
+rm -f ${jdkbinary}
+
+# set jdk 10 home environment variables.
+JAVA_HOME=/usr/local/java/${jdkhome}
+export JAVA_HOME
+PATH=${JAVA_HOME}/bin:$PATH
+export PATH
+
+# verify installation.
+java -version
