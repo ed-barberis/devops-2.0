c1cce86aeb9af01718300567b88f823806a5380f
Ed Barberis <ed.barberis@gmail.com>
2018-05-07 15:55:40 -0400

    Fixed error in GitLab installer.

--- provisioners/scripts/centos/install_centos7_gitlab.sh
+++ provisioners/scripts/centos/install_centos7_gitlab.sh
@@ -1,12 +1,27 @@
 #!/bin/sh -eux
 # install gitlab git repository management platform on centos linux 7.x.
 
+# install gitlab pre-requisites.
+yum -y install curl policycoreutils-python openssh-server openssh-clients
+#systemctl enable sshd
+#systemctl start sshd
+#firewall-cmd --permanent --add-service=http
+#systemctl reload firewalld
+
+# install postfix to send notification emails.
+# If you want to use another solution to send emails, please skip this step and
+# configure an external smtp server after gitlab has been installed.
+#yum -y install postfix
+#systemctl enable postfix
+#systemctl start postfix
+
 # install gitlab platform. -----------------------------------------------------
 curl --silent https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | bash
-yum -y install gitlab-ce
+EXTERNAL_URL="http://10.0.2.15" yum -y install gitlab-ce
+#yum -y install gitlab-ce
 
 # configure gitlab.
-gitlab-ctl reconfigure
+#gitlab-ctl reconfigure
 
 # display network configuration. -----------------------------------------------
-ifconfig
+#ip addr show
