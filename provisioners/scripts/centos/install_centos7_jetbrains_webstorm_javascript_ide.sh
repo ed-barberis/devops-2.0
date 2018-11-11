6372b7a9d2fea35700f3b6e3c4480777141abb8e
Ed Barberis <ed.barberis@gmail.com>
2018-11-11 17:48:35 -0500

    Upgraded JetBrains WebStorm to version 2018.2.5.

--- provisioners/scripts/centos/install_centos7_jetbrains_webstorm_javascript_ide.sh
+++ provisioners/scripts/centos/install_centos7_jetbrains_webstorm_javascript_ide.sh
@@ -6,8 +6,8 @@ devops_home="${devops_home:-/opt/devops}"                   # [optional] devops 
 
 # install webstorm javascript ide. ---------------------------------------------
 webstorm_home="webstorm"
-webstorm_release="2018.2.4"
-webstorm_build="182.4505.50"
+webstorm_release="2018.2.5"
+webstorm_build="182.4892.25"
 
 webstorm_binary="WebStorm-${webstorm_release}.tar.gz"
 webstorm_folder="WebStorm-${webstorm_build}"
