4dce656e6369294d0a10e748d5dd3f7fbeaa8bf4
Ed Barberis <ed.barberis@gmail.com>
2019-01-05 17:13:53 -0500

    Upgraded Git to 2.20.1.

--- provisioners/scripts/centos/install_centos7_git.sh
+++ provisioners/scripts/centos/install_centos7_git.sh
@@ -7,7 +7,7 @@ yum -y install gcc perl-ExtUtils-MakeMaker
 
 # install git binaries from source. --------------------------------------------
 githome="git"
-gitrelease="2.19.1"
+gitrelease="2.20.1"
 gitfolder="git-${gitrelease}"
 gitbinary="${gitfolder}.tar.gz"
 
