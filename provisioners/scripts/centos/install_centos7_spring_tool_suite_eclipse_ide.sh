fe14aac81061d006b8ec4efd9ae55fce040d5829
Ed Barberis <ed.barberis@gmail.com>
2019-01-28 15:31:18 -0500

    Upgraded Spring Tool Suite IDE to 4.1.1 (Eclipse 2018-12 [4.10.0]).

--- provisioners/scripts/centos/install_centos7_spring_tool_suite_eclipse_ide.sh
+++ provisioners/scripts/centos/install_centos7_spring_tool_suite_eclipse_ide.sh
@@ -8,8 +8,8 @@ devops_home="${devops_home:-/opt/devops}"                   # [optional] devops 
 
 # install spring tool suite ide. -----------------------------------------------
 stshome="sts"
-stsrelease="4.1.0"
-stsnumber="1473841533"
+stsrelease="4.1.1"
+stsnumber="1013671300"
 eclipseversion="2018-12"
 eclipserelease="4.10.0"
 
