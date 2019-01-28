9792a29dece1691615357043bd4a7e43f6abec74
Ed Barberis <ed.barberis@gmail.com>
2019-01-28 15:13:20 -0500

    Upgraded MySQL Shell to version 8.0.14.

--- provisioners/scripts/centos/install_centos7_oracle_mysql_shell_8.sh
+++ provisioners/scripts/centos/install_centos7_oracle_mysql_shell_8.sh
@@ -1,28 +1,28 @@
 #!/bin/sh -eux
 # install oracle mysql shell command-line utility on centos linux 7.x.
 
-# set default value for devops home environment variable if not set. -----------
+# set default value for devops home environment variable if not set. -------------------------------
 devops_home="${devops_home:-/opt/devops}"                   # [optional] devops home (defaults to '/opt/devops').
 
-# create scripts directory (if needed). ----------------------------------------
+# create scripts directory (if needed). ------------------------------------------------------------
 mkdir -p ${devops_home}/provisioners/scripts/centos
 cd ${devops_home}/provisioners/scripts/centos
 
-# install mysql shell. ---------------------------------------------------------
-mysqlsh_release="8.0.13-1"
+# install mysql shell. -----------------------------------------------------------------------------
+mysqlsh_release="8.0.14-1"
 mysqlsh_binary="mysql-shell-${mysqlsh_release}.el7.x86_64.rpm"
 
 # download mysql shell repository.
 rm -f ${mysqlsh_binary}
 wget --no-verbose --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" https://dev.mysql.com/get/Downloads/MySQL-Shell/${mysqlsh_binary}
 
-# install mysql shell. ---------------------------------------------------------
+# install mysql shell. -----------------------------------------------------------------------------
 yum -y install ${mysqlsh_binary}
 
 # verify mysql shell installation.
 mysqlsh --version
 
-# mysqlsh command-line examples. -----------------------------------------------
+# mysqlsh command-line examples. -------------------------------------------------------------------
 # MySQL Shell User Guide
 #   This is the MySQL Shell User Guide extract from the MySQL 8.0 Reference Manual.
 #   https://dev.mysql.com/doc/mysql-shell-excerpt/8.0/en/
