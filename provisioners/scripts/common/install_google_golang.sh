46ca0a60089f0a16d1f5609b762635b77ca5f510
Ed Barberis <ed.barberis@gmail.com>
2019-02-26 16:33:55 -0500

    Upgraded Ansible and Go to latest versions.

--- provisioners/scripts/common/install_google_golang.sh
+++ provisioners/scripts/common/install_google_golang.sh
@@ -3,7 +3,7 @@
 
 # install go programming language. ---------------------------------------------
 gohome="go"
-gorelease="1.11.5"
+gorelease="1.12"
 gobinary="${gohome}${gorelease}.linux-amd64.tar.gz"
 gofolder="${gohome}-${gorelease}"
 
