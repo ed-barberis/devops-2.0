21636d3ce7d9bbd429cd5fe6fe79ca5e76e19f2e
Ed Barberis <ed.barberis@gmail.com>
2019-04-09 08:26:35 -0400

    Upgrade Go to v1.12.3.

--- provisioners/scripts/common/install_google_golang.sh
+++ provisioners/scripts/common/install_google_golang.sh
@@ -3,7 +3,7 @@
 
 # install go programming language. ---------------------------------------------
 gohome="go"
-gorelease="1.12.1"
+gorelease="1.12.3"
 gobinary="${gohome}${gorelease}.linux-amd64.tar.gz"
 gofolder="${gohome}-${gorelease}"
 
