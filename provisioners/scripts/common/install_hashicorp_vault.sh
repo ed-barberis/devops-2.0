716d7bc0451fc1d205bd18e240ba8c69949ae257
Ed Barberis <ed.barberis@gmail.com>
2019-07-29 11:28:05 -0400

    Upgraded Ansible and Hashicorp utilities to latest versions.

--- provisioners/scripts/common/install_hashicorp_vault.sh
+++ provisioners/scripts/common/install_hashicorp_vault.sh
@@ -2,7 +2,7 @@
 # install vault tool for managing secrets by hashicorp.
 
 # install hashicorp vault. -----------------------------------------------------
-vault_release="1.1.3"
+vault_release="1.1.5"
 vault_binary="vault_${vault_release}_linux_amd64.zip"
 
 # create local bin directory (if needed).
