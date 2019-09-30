dabf583fea29398fdabd42dd94996e66095f521e
Ed Barberis <ed.barberis@gmail.com>
2019-09-30 16:32:52 -0400

    Upgraded Go, Scala, Scala SBT, and Hashicorp Terraform to latest versions.

--- provisioners/scripts/common/install_google_golang.sh
+++ provisioners/scripts/common/install_google_golang.sh
@@ -3,7 +3,7 @@
 
 # install go programming language. ---------------------------------------------
 gohome="go"
-gorelease="1.13"
+gorelease="1.13.1"
 gobinary="${gohome}${gorelease}.linux-amd64.tar.gz"
 gofolder="${gohome}-${gorelease}"
 
