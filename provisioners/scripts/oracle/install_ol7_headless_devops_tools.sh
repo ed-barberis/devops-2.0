5b7ef597239858c93f7e91f19e6c1233043c19c0
Ed Barberis <ed.barberis@gmail.com>
2019-10-09 10:28:51 -0400

    Updated EPEL repository install for Oracle Linux 7.7.

--- provisioners/scripts/oracle/install_ol7_headless_devops_tools.sh
+++ provisioners/scripts/oracle/install_ol7_headless_devops_tools.sh
@@ -1,13 +1,6 @@
 #!/bin/sh -eux
 # install useful headless (command-line) developer tools.
 
-# set default value for devops home environment variable if not set. -------------------------------
-devops_home="${devops_home:-/opt/devops}"   # [optional] devops home (defaults to '/opt/devops').
-
-# create scripts directory (if needed). ------------------------------------------------------------
-mkdir -p ${devops_home}/provisioners/scripts/oracle
-cd ${devops_home}/provisioners/scripts/oracle
-
 # install epel repository. -------------------------------------------------------------------------
 yum -y install oracle-epel-release-el7
 yum-config-manager --enable ol7_developer_EPEL
