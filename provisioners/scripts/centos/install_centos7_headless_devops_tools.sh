f6ae00585eddc179ce8439da1ee29f6e7492d8e0
Ed Barberis <ed.barberis@gmail.com>
2019-10-17 09:24:35 -0400

    Updated EPEL repository install for CentOS 7.7.

--- provisioners/scripts/centos/install_centos7_headless_devops_tools.sh
+++ provisioners/scripts/centos/install_centos7_headless_devops_tools.sh
@@ -1,21 +1,10 @@
 #!/bin/sh -eux
 # install useful headless (command-line) developer tools.
 
-# set default value for devops home environment variable if not set. -----------
-devops_home="${devops_home:-/opt/devops}"                   # [optional] devops home (defaults to '/opt/devops').
+# install epel repository. -------------------------------------------------------------------------
+yum -y install epel-release
 
-# create scripts directory (if needed). ----------------------------------------
-mkdir -p ${devops_home}/provisioners/scripts/centos
-cd ${devops_home}/provisioners/scripts/centos
-
-# install epel repository if needed. -------------------------------------------
-if [ ! -f "/etc/yum.repos.d/epel.repo" ]; then
-  rm -f install epel-release-latest-7.noarch.rpm
-  wget --no-verbose https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-  yum -y install epel-release-latest-7.noarch.rpm
-fi
-
-# install python 2.x pip and setuptools. ---------------------------------------
+# install python 2.x pip and setuptools. -----------------------------------------------------------
 yum -y install python-pip
 python --version
 pip --version
@@ -29,13 +18,13 @@ yum -y install python-setuptools
 pip install --upgrade setuptools
 easy_install --version
 
-# install software collections library. (needed later for python 3.x.) ---------
+# install software collections library. (needed later for python 3.x.) -----------------------------
 yum -y install scl-utils
 yum -y install centos-release-scl
 
-# install git. -----------------------------------------------------------------
+# install git. -------------------------------------------------------------------------------------
 yum -y install git
 git --version
 
-# install bash completion tools. -----------------------------------------------
+# install bash completion tools. -------------------------------------------------------------------
 yum -y install bash-completion bash-completion-extras
