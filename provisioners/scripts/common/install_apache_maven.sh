ed2aac22281084a6ea9981ebe3f66888c3b7a9c7
Ed Barberis <ed.barberis@gmail.com>
2019-11-27 18:27:33 -0500

    Upgraded Apache Maven, Scala SBT, and Hashicorp Terraform to latest versions.

--- provisioners/scripts/common/install_apache_maven.sh
+++ provisioners/scripts/common/install_apache_maven.sh
@@ -3,7 +3,7 @@
 
 # install apache maven. --------------------------------------------------------
 m2home="apache-maven"
-mvnrelease="3.6.2"
+mvnrelease="3.6.3"
 mvnfolder="${m2home}-${mvnrelease}"
 mvnbinary="${mvnfolder}-bin.tar.gz"
 
