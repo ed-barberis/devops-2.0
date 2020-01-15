3ef4ed8b43d8d2a1dc469d791fc0714870f7346e
Ed Barberis <ed.barberis@gmail.com>
2020-01-15 10:26:17 -0500

    Updated support for Docker Compose Completion for Bash to version 1.25.1.

--- provisioners/scripts/common/install_headless_user_env.sh
+++ provisioners/scripts/common/install_headless_user_env.sh
@@ -8,7 +8,7 @@ user_home="${user_home:-/home/$user_name}"
 user_docker_profile="${user_docker_profile:-false}"
 user_prompt_color="${user_prompt_color:-green}"
 d_completion_release="${d_completion_release:-19.03.5}"
-dc_completion_release="${dc_completion_release:-1.25.0}"
+dc_completion_release="${dc_completion_release:-1.25.1}"
 
 # set default value for devops home environment variable if not set. -------------------------------
 devops_home="${devops_home:-/opt/devops}"
@@ -29,7 +29,7 @@ Usage:
                                                                 #              'black', 'blue', 'cyan', 'green', 'magenta', 'red', 'white', 'yellow'
                                                                 #
     [root]# export d_completion_release="19.03.5"               # [optional] docker completion for bash release (defaults to '19.03.5').
-    [root]# export dc_completion_release="1.25.0"               # [optional] docker compose completion for bash release (defaults to '1.25.0').
+    [root]# export dc_completion_release="1.25.1"               # [optional] docker compose completion for bash release (defaults to '1.25.1').
     [root]# export devops_home="/opt/devops"                    # [optional] devops home (defaults to '/opt/devops').
     [root]# $0
 EOF
