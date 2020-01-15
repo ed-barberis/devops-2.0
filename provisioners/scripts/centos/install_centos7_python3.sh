6664c7088197a54012e972a6d06855af217da224
Ed Barberis <ed.barberis@gmail.com>
2020-01-15 10:13:27 -0500

    Modified the default python environment to be Python 3.

--- provisioners/scripts/centos/install_centos7_python3.sh
+++ provisioners/scripts/centos/install_centos7_python3.sh
@@ -1,24 +1,17 @@
 #!/bin/sh -eux
-# install python 3.6 from the software collection library for linux 7.x..
+# install python 3.x for centos 7.x.
 
-# set default value for devops home environment variable if not set. -------------------------------
-devops_home="${devops_home:-/opt/devops}"                   # [optional] devops home (defaults to '/opt/devops').
+# set default values for input environment variables if not set. -----------------------------------
+# [OPTIONAL] python 3.x install parameters [w/ defaults].
+user_name="${user_name:-centos}"
 
-# create scripts directory (if needed). ------------------------------------------------------------
-mkdir -p ${devops_home}/provisioners/scripts/centos
-cd ${devops_home}/provisioners/scripts/centos
-
-# install python 3.6. ------------------------------------------------------------------------------
-yum -y install rh-python36
+# install python 3.x. ------------------------------------------------------------------------------
+yum -y install python3
 
 # verify installation.
-scl enable rh-python36 -- python --version
+python3 --version
+pip3 --version
 
-# install python 3.6 pip. --------------------------------------------------------------------------
-rm -f get-pip.py
-wget --no-verbose https://bootstrap.pypa.io/get-pip.py
-scl enable rh-python36 -- python ${devops_home}/provisioners/scripts/centos/get-pip.py
-
-# verify installation.
-scl enable rh-python36 -- pip --version
-scl enable rh-python36 -- pip3 --version
+# upgrade python 3.x pip. --------------------------------------------------------------------------
+runuser -c "PATH=/home/${user_name}/.local/bin:/usr/local/bin:${PATH} pip3 install pip --upgrade --user" - ${user_name}
+runuser -c "PATH=/home/${user_name}/.local/bin:/usr/local/bin:${PATH} pip3 --version" - ${user_name}
