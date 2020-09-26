fcf2627ba2592fddfa916267679a23f778a4a4b8
Ed Barberis <ed.barberis@gmail.com>
2020-09-26 09:26:34 -0400

    Upgraded AppDynamics Java Agent JDK8+ to 20.9.0.30985.

--- provisioners/scripts/common/install_appdynamics_java_agent.sh
+++ provisioners/scripts/common/install_appdynamics_java_agent.sh
@@ -27,7 +27,8 @@ appd_controller_root_password="${appd_controller_root_password:-welcome1}"
 set -x  # turn command display back ON.
 appd_java_agent_home="${appd_java_agent_home:-appagent}"
 appd_java_agent_user="${appd_java_agent_user:-vagrant}"
-appd_java_agent_release="${appd_java_agent_release:-20.3.0.29587}"
+appd_java_agent_release="${appd_java_agent_release:-20.9.0.30985}"
+appd_java_agent_sha256="${appd_java_agent_sha256:-494819db178ba88b48f69e734f4efb8f0bce08e1dcabce98e95b2f474cf01fdd}"
 
 # [OPTIONAL] appdynamics java agent config parameters [w/ defaults].
 appd_java_agent_config="${appd_java_agent_config:-false}"
@@ -61,7 +62,9 @@ Usage:
     [root]# export appd_controller_root_password="welcome1"             # [optional] controller root password (defaults to 'welcome1').
     [root]# export appd_java_agent_home="appagent"                      # [optional] java agent home (defaults to 'appagent').
     [root]# export appd_java_agent_user="vagrant"                       # [optional] java agent user (defaults to user 'vagrant').
-    [root]# export appd_java_agent_release="20.3.0.29587"               # [optional] java agent release (defaults to '20.3.0.29587').
+    [root]# export appd_java_agent_release="20.9.0.30985"               # [optional] java agent release (defaults to '20.9.0.30985').
+                                                                        # [optional] java agent sha-256 checksum (defaults to published value).
+    [root]# export appd_java_agent_sha256="494819db178ba88b48f69e734f4efb8f0bce08e1dcabce98e95b2f474cf01fdd"
 
   [OPTIONAL] appdynamics java agent config parameters [w/ defaults].
     [root]# export appd_java_agent_config="true"                        # [optional] configure appd java agent? [boolean] (defaults to 'false').
@@ -73,7 +76,7 @@ Usage:
           environment.
 
           In either case, you will need to validate the configuration before starting the Java Agent. The
-          configuration file can be found here: '<java_agent_home>/appagent/ver20.3.0.29587/conf/controller-info.xml'
+          configuration file can be found here: '<java_agent_home>/appagent/ver20.9.0.30985/conf/controller-info.xml'
 
     [root]# export appd_controller_host="apm"                           # [optional] controller host (defaults to 'apm').
     [root]# export appd_controller_port="8090"                          # [optional] controller port (defaults to '8090').
@@ -108,8 +111,7 @@ set -x  # turn command display back ON.
 
 # set appdynamics java agent installation variables. -----------------------------------------------
 appd_java_agent_folder="${appd_java_agent_home}-${appd_java_agent_release}"
-appd_java_agent_binary="AppServerAgent-${appd_java_agent_release}.zip"
-appd_java_agent_sha256="e7631bc87e99ffe259d04ad60bcc8559aa46fbee808295955886c820821c798d"
+appd_java_agent_binary="AppServerAgent-1.8-${appd_java_agent_release}.zip"
 
 # create appdynamics java agent parent folder. -----------------------------------------------------
 mkdir -p ${appd_home}/${appd_java_agent_folder}
@@ -140,7 +142,7 @@ oauth_token=$(awk -F '"' '{print $10}' ${oauth_token_filename})
 
 # download the appdynamics java agent binary.
 rm -f ${appd_java_agent_binary}
-curl --silent --location --remote-name --header "Authorization: Bearer ${oauth_token}" https://download.appdynamics.com/download/prox/download-file/sun-jvm/${appd_java_agent_release}/${appd_java_agent_binary}
+curl --silent --location --remote-name --header "Authorization: Bearer ${oauth_token}" https://download.appdynamics.com/download/prox/download-file/java-jdk8/${appd_java_agent_release}/${appd_java_agent_binary}
 chmod 644 ${appd_java_agent_binary}
 
 rm -f ${post_data_filename}
