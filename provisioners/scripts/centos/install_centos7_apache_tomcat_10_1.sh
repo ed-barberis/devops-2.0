7d7b4674c1b457e5513fe097f09ad41ba2359213
Ed Barberis <ed.barberis@gmail.com>
2023-02-09 14:11:40 -0500

    Upgraded support for Apache Tomcat 8.5.85, 9.0.71, and 10.1.5.

--- provisioners/scripts/centos/install_centos7_apache_tomcat_10_1.sh
+++ provisioners/scripts/centos/install_centos7_apache_tomcat_10_1.sh
@@ -22,9 +22,9 @@
 # set default values for input environment variables if not set. -----------------------------------
 # [OPTIONAL] tomcat web server install parameters [w/ defaults].
 tomcat_home="${tomcat_home:-apache-tomcat-10.1}"                    # [optional] tomcat home (defaults to 'apache-tomcat-10.1').
-tomcat_release="${tomcat_release:-10.1.2}"                          # [optional] tomcat release (defaults to '10.1.2').
+tomcat_release="${tomcat_release:-10.1.5}"                          # [optional] tomcat release (defaults to '10.1.5').
                                                                     # [optional] tomcat sha-512 checksum (defaults to published value).
-tomcat_sha512="${tomcat_sha512:-50e933934cbf2915603264dfd245db0137491555d71d4fe8a535a7a8b6bc36bb4984e8bb2f38c968fbfc84a87aeeaa773ce1c6ee5f2eaa87780d1869251da51e}"
+tomcat_sha512="${tomcat_sha512:-7fa381d1344ca51d9425567575a30f6d8ca3279234aa00a59f85cb589f00f35274de873e2ade2030599074e65d39d2ad6b1bd445b01cde0eb59bb19799631c4b}"
 tomcat_username="${tomcat_username:-vagrant}"                       # [optional] tomcat user name (defaults to 'vagrant').
 tomcat_group="${tomcat_group:-vagrant}"                             # [optional] tomcat group (defaults to 'vagrant').
 
