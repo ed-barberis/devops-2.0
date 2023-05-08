819fe800b85b86387f354ca5011085457200fb4f
Ed Barberis <ed.barberis@gmail.com>
2023-05-08 12:06:50 -0400

    Upgraded Spring Tool Suite IDE to 4.18.1 (Eclipse 2023-03 [4.27.0]).

--- provisioners/scripts/centos/install_centos7_spring_tool_suite_eclipse_ide.sh
+++ provisioners/scripts/centos/install_centos7_spring_tool_suite_eclipse_ide.sh
@@ -8,17 +8,16 @@ devops_home="${devops_home:-/opt/devops}"                   # [optional] devops 
 
 # install spring tool suite ide. -------------------------------------------------------------------
 sts_home="sts"
-sts_release="4.18.0"
+sts_release="4.18.1"
 sts_number="1981140011"
 eclipse_version="2023-03"
 eclipse_release="4.27.0"
-
 eclipse_dist=$(echo "e${eclipse_release}" | awk -F "." '{printf "%s.%s", $1, $2}')
 sts_family="${sts_release:0:1}"
 sts_folder="${sts_home}-${sts_release}.RELEASE"
 sts_config="SpringToolSuite${sts_family}.ini"
 sts_binary="spring-tool-suite-${sts_family}-${sts_release}.RELEASE-e${eclipse_release}-linux.gtk.x86_64.tar.gz"
-sts_sha256="71588b58475adccef06724684565e437970e0f624d04eeeab649f4839636ab32"
+sts_sha256="cddfb286ee41a7fc0fc4ceac8f341817627fe473169ff800c81ce083bb4f998c"
 
 # create spring tool suite home parent folder.
 mkdir -p /usr/local/spring
