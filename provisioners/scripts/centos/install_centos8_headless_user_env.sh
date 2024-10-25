fec3e7b9e20781a9f34ad13eb0fbf5f94fb5047a
Ed Barberis <ed.barberis@gmail.com>
2024-10-25 05:11:44 -0400

    Removed bug fix for Vundle install scripts for Vim (Plugin issue resolved).

--- provisioners/scripts/centos/install_centos8_headless_user_env.sh
+++ provisioners/scripts/centos/install_centos8_headless_user_env.sh
@@ -130,39 +130,40 @@ chown ${user_name}:${user_group} ${vimrc_local}
 # download and install useful vim configuration based on developer pair stations at pivotal labs.
 runuser -c "git clone https://github.com/pivotal-legacy/vim-config.git ${user_home}/.vim" - ${user_name}
 
-# vundle plugin url bug fix. -----------------------------------------------------------------------
-# define vundle search and replace strings for the stream editor.
-vundle_string_search="VundleVim"
-vundle_string_replace="ed-barberis"
+###### vundle plugin url bug fix. -----------------------------------------------------------------------
+###### define vundle search and replace strings for the stream editor.
+#####vundle_string_search="VundleVim"
+#####vundle_string_replace="ed-barberis"
+#####
+###### modify the installer to point to the new vundle vim repository.
+#####vundle_install_file="${vimrc_home}/bin/install"
+#####if [ -f "$vundle_install_file" ]; then
+#####  # copy the original file using the current date.
+#####  runuser -c "cp -p ${vundle_install_file} ${vundle_install_file}.${curdate}.orig1" - ${user_name}
+#####
+#####  # use the stream editor to modify the plugin repository url.
+#####  if ! grep -qF -- "${vundle_string_replace}" "${vundle_install_file}" ; then
+#####    runuser -c "sed -i -e \"s/${vundle_string_search}/${vundle_string_replace}/g\" ${vundle_install_file}" - ${user_name}
+#####  fi
+#####fi
+#####
+###### modify the vim config to point to the new vundle vim plugin.
+#####vim_config_file="${vimrc_home}/vimrc"
+#####if [ -f "$vim_config_file" ]; then
+#####  # copy the original file using the current date.
+#####  runuser -c "cp -p ${vim_config_file} ${vim_config_file}.${curdate}.orig1" - ${user_name}
+#####
+#####  # use the stream editor to modify the plugin.
+#####  if ! grep -qF -- "${vundle_string_replace}" "${vim_config_file}" ; then
+#####    runuser -c "sed -i -e \"s/${vundle_string_search}/${vundle_string_replace}/g\" ${vim_config_file}" - ${user_name}
+#####  fi
+#####fi
 
-# modify the installer to point to the new vundle vim repository.
-vundle_install_file="${vimrc_home}/bin/install"
-if [ -f "$vundle_install_file" ]; then
-  # copy the original file using the current date.
-  runuser -c "cp -p ${vundle_install_file} ${vundle_install_file}.${curdate}.orig1" - ${user_name}
-
-  # use the stream editor to modify the plugin repository url.
-  if ! grep -qF -- "${vundle_string_replace}" "${vundle_install_file}" ; then
-    runuser -c "sed -i -e \"s/${vundle_string_search}/${vundle_string_replace}/g\" ${vundle_install_file}" - ${user_name}
-  fi
-fi
-
-# modify the vim config to point to the new vundle vim plugin.
-vim_config_file="${vimrc_home}/vimrc"
-if [ -f "$vim_config_file" ]; then
-  # copy the original file using the current date.
-  runuser -c "cp -p ${vim_config_file} ${vim_config_file}.${curdate}.orig1" - ${user_name}
-
-  # use the stream editor to modify the plugin.
-  if ! grep -qF -- "${vundle_string_replace}" "${vim_config_file}" ; then
-    runuser -c "sed -i -e \"s/${vundle_string_search}/${vundle_string_replace}/g\" ${vim_config_file}" - ${user_name}
-  fi
-fi
-
-# add the terraform plugin into the vim config. ----------------------------------------------------
+# add the terraform and copilot plugins into the vim config. ---------------------------------------
 # define vim search and replace strings for the stream editor.
 vim_config_search="  Plugin 'luan\/vim-concourse'"
-vim_config_line="  Plugin 'hashivim\/vim-terraform'"
+vim_config_line_01="  Plugin 'hashivim\/vim-terraform'"
+vim_config_line_02="  Plugin 'github\/copilot.vim'"
 
 # modify the vim config to add the terraform plugin.
 vim_config_file="${vimrc_home}/vimrc"
@@ -172,7 +173,7 @@ if [ -f "$vim_config_file" ]; then
 
   # use the stream editor to add the plugin.
   if ! grep -qF -- 'terraform' "${vim_config_file}" ; then
-    runuser -c "sed -i -e \"s/^${vim_config_search}$/${vim_config_search}\n${vim_config_line}/g\" ${vim_config_file}" - ${user_name}
+    runuser -c "sed -i -e \"s/^${vim_config_search}$/${vim_config_search}\n${vim_config_line_01}\n${vim_config_line_02}/g\" ${vim_config_file}" - ${user_name}
   fi
 fi
 
