74f484d59a42505ed55eeaeb53277030ef404f28
Ed Barberis <ed.barberis@gmail.com>
2024-10-25 05:11:44 -0400

    Added support for Neofetch v7.1.0 command-line system information tool.

---
+++ provisioners/scripts/centos/install_centos9_headless_devops_tools.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -eux
+# install useful headless (command-line) developer tools on centos 9.
+
+# install epel repository. -------------------------------------------------------------------------
+dnf -y install epel-release
+dnf repolist
+
+# install neofetch system information tool. ---------------------------------------------------------
+dnf -y install neofetch
+
+# verify installation.
+set +e  # temporarily turn 'exit pipeline on non-zero return status' OFF.
+neofetch --version
+set -e  # turn 'exit pipeline on non-zero return status' back ON.
+
+# display system information.
+neofetch
+
+# install git. -------------------------------------------------------------------------------------
+dnf -y install git
+git --version
