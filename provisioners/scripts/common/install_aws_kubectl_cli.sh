e3deae98985d760bf2a60a538e3aaff67df0e364
Ed Barberis <ed.barberis@gmail.com>
2025-02-18 13:48:18 -0500

    Upgraded support to AWS 'kubectl' for [2024-12-20].

--- provisioners/scripts/common/install_aws_kubectl_cli.sh
+++ provisioners/scripts/common/install_aws_kubectl_cli.sh
@@ -18,22 +18,24 @@
 cpu_arch=$(uname -m)
 
 # install kubectl cli. -----------------------------------------------------------------------------
-#kubectl_release="1.31.2"
-#kubectl_date="2024-11-15"
-kubectl_release="1.30.6"
-kubectl_date="2024-11-15"
+#kubectl_release="1.32.0"
+#kubectl_date="2024-12-20"
+kubectl_release="1.31.3"
+kubectl_date="2024-12-12"
+#kubectl_release="1.30.7"
+#kubectl_date="2024-12-12"
 #kubectl_release="1.29.10"
-#kubectl_date="2024-11-15"
+#kubectl_date="2024-12-12"
 #kubectl_release="1.28.15"
-#kubectl_date="2024-11-15"
+#kubectl_date="2024-12-12"
 #kubectl_release="1.27.16"
-#kubectl_date="2024-11-15"
+#kubectl_date="2024-12-12"
 #kubectl_release="1.26.15"
-#kubectl_date="2024-11-15"
+#kubectl_date="2024-12-12"
 #kubectl_release="1.25.16"
-#kubectl_date="2024-11-15"
+#kubectl_date="2024-12-12"
 #kubectl_release="1.24.17"
-#kubectl_date="2024-11-15"
+#kubectl_date="2024-12-12"
 #kubectl_release="1.23.17"
 #kubectl_date="2024-09-11"
 
@@ -43,14 +45,15 @@ declare -A sha256_values_array
 # set the kubectl cli binary and sha256 values based on cpu architecture.
 if [ "$cpu_arch" = "x86_64" ]; then
   # set the amd64 sha256 values.
-  sha256_values_array["1.31.2"]="e9f1ef5bd7098d0e0f4d0690f46b4a822396726d59f96b397c18300ea727c4f5"
-  sha256_values_array["1.30.6"]="711f93f24a434c0f6abf675cda994a22862c7e13c0fd36be4e8e8f2599c08dc9"
-  sha256_values_array["1.29.10"]="850b3655577f60d93b458d550ae3d2783884a08f26edfa258066a5737f514ae3"
-  sha256_values_array["1.28.15"]="398dc2651ac8d3fe0cbfff4af4c29a6a7297a5a76624b70410f035a35443d8cb"
-  sha256_values_array["1.27.16"]="5f51172d2c0fc12d4478cdf221469b29db1bc82033b358552b594138ccd07f9d"
-  sha256_values_array["1.26.15"]="d91fb220b663f9afda59dfbe1b35db73bffb441d8da11d5da8bab1da7da65b96"
-  sha256_values_array["1.25.16"]="f88c48abeca88b474232c759e4b02ec30e71f195fcb025a016e349d6b7a88778"
-  sha256_values_array["1.24.17"]="0cbfaded56af3e303a4d0f22d659831f6c85223db506f0b3614943bf86407e97"
+  sha256_values_array["1.32.0"]="3d46610a1eba3dd47e4879f69effa481f56517d7a4a57bb3ebf7498f4df2e99e"
+  sha256_values_array["1.31.3"]="9b14b506556ce59800319128a591a0a04b32196131e672e0f13b8e91ed611128"
+  sha256_values_array["1.30.7"]="5754717b7a6dd931245e4643b867701f50e7f5a4c93626216c4b738af45e6866"
+  sha256_values_array["1.29.10"]="260fe18665dab11f5b075c11003e87a606cf4c2418e723a41f9218505c8fc8b6"
+  sha256_values_array["1.28.15"]="12e36b5275ff9510064bb7d27d54f53c43dfab080917a9e28493fb629a0f9938"
+  sha256_values_array["1.27.16"]="1813737d0997f372a1be2da6897a638e2a7eb81e5f828e0e0e724f05c50256aa"
+  sha256_values_array["1.26.15"]="4dea29aaca9314d089bd8b1829f9c3dec02618c2e44064e92271559175811e24"
+  sha256_values_array["1.25.16"]="f8850275ba4f5fbd15474a5ccf5903ab80447ca5396841a2b17ab7ddddf6a114"
+  sha256_values_array["1.24.17"]="f4e5ccff4b212507ff2864947b04fcbf68d68cb0f1c4a562fa33e301907c3626"
   sha256_values_array["1.23.17"]="23b46e400d3594d7de611e116746269656d828db7de129c86767a6acf7322d1f"
 
   # set the amd64 download path.
@@ -58,14 +61,15 @@ if [ "$cpu_arch" = "x86_64" ]; then
 
 elif [ "$cpu_arch" = "aarch64" ]; then
   # set the arm64 sha256 values.
-  sha256_values_array["1.31.2"]="e9443d9f523387d5d0b7540e96ee254528bd385f063a10c17a6cc672e2b34cbb"
-  sha256_values_array["1.30.6"]="8eec44c56e641eb22b6439a76afabe07a972cc2e8ca2326db49ef6ec90911d66"
-  sha256_values_array["1.29.10"]="56efe1dcc9ceac1b4d6b88909e956d8aed6f586896513ed0dd25a7f8e5d663f2"
-  sha256_values_array["1.28.15"]="f2a66f83529aba78677f3ea75a313697561a8b2624aa97e9aa68e38e33c1a404"
-  sha256_values_array["1.27.16"]="e9c67c57d236848998932235bce13e729930c002eee7148e6eb4ccf6037286d3"
-  sha256_values_array["1.26.15"]="874bb6730348d383451adb117daefb5c117d39e372d129bbae60b49ef9222f14"
-  sha256_values_array["1.25.16"]="91f15cd07017f4d26f90563fccb96dfded26bed6786928ad259d3a5a025d4d2d"
-  sha256_values_array["1.24.17"]="15beb6db053fb7ec86ad6c66514a67d809668cd990858b7a30b11010de87eedf"
+  sha256_values_array["1.32.0"]="d53a6d7f902809e3e8114801903c1f145ab40e1cb4735d9921559620ac1a0988"
+  sha256_values_array["1.31.3"]="326327b6449bc0880ab2c173faeb00037ad3d1daa4b8437e3b90f918d631aee5"
+  sha256_values_array["1.30.7"]="1ab4d6b90134ea845b478f7596cb051c4a2384d1db1135d000530d27fbbbdab2"
+  sha256_values_array["1.29.10"]="fbf741880f192e640da1c32b2af6dab5f84d0c58c55f0282fc92b4fb65fef001"
+  sha256_values_array["1.28.15"]="ffc372c4bc25f803114ccbbebcc3541dae0e6a7b135125ab8a245a0679606725"
+  sha256_values_array["1.27.16"]="7e103cb0081e88eeccfcae2e9c4616135b289558f5b4fe644fab21a52d36c8c8"
+  sha256_values_array["1.26.15"]="f974aee8355790d6b9848c42d64898308a2e2c084c3437a5d720c6444e317db3"
+  sha256_values_array["1.25.16"]="329b919f9857f5fe35481d2eb5b1ea30c3a504e39505f540228dd631e0b6b5e0"
+  sha256_values_array["1.24.17"]="882ca847ba68622416a1e70b2a17ae28febf82e8da0ef1b6d94084fcb65329d9"
   sha256_values_array["1.23.17"]="721a76d9f9d60cbfaafec1a88f554d324c6750517d17b0d1c59e53ef3483f6f5"
 
   # set the arm64 download path.
