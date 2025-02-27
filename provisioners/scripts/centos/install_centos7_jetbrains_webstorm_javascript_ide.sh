444fef2e4c1c1178362fe6525973572144de01d7
Ed Barberis <ed.barberis@gmail.com>
2025-02-27 15:58:48 -0500

    Upgraded JetBrains WebStorm to versions 2024.3.4.

--- provisioners/scripts/centos/install_centos7_jetbrains_webstorm_javascript_ide.sh
+++ provisioners/scripts/centos/install_centos7_jetbrains_webstorm_javascript_ide.sh
@@ -9,19 +9,19 @@ cpu_arch=$(uname -m)
 
 # install webstorm javascript ide. -----------------------------------------------------------------
 webstorm_home="webstorm"
-webstorm_release="2024.3.3"
-webstorm_build="243.24978.60"
+webstorm_release="2024.3.4"
+webstorm_build="243.25659.40"
 webstorm_folder="WebStorm-${webstorm_build}"
 
 # set the webstorm binary and sha256 values based on cpu architecture.
 if [ "$cpu_arch" = "x86_64" ]; then
   # set the amd64 variables.
   webstorm_binary="WebStorm-${webstorm_release}.tar.gz"
-  webstorm_sha256="bcca8a7f510e19ae88b128ecfd099ce8319986ddb71e29ee89dcdd1362c964c9"
+  webstorm_sha256="206a5e6b6fd3e603d62a7bbb26b0bf3b7ff7939b3e0cd136d360cdb80ec455f8"
 elif [ "$cpu_arch" = "aarch64" ]; then
   # set the arm64 variables.
   webstorm_binary="WebStorm-${webstorm_release}-aarch64.tar.gz"
-  webstorm_sha256="2b056893b500ed2275d20bc08f2a1cbacd3b25ad8f4543b7b123778fe53fe856"
+  webstorm_sha256="498efb5b3e79a0f71dc7f44f54a2155a17b5d53d8b7a71ae6aaef87abc59bfce"
 else
   echo "Error: Unsupported CPU architecture: '${cpu_arch}'."
   exit 1
