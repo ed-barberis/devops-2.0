94f077baeac2574bf8856bffc5996daf1feb9ed3
Ed Barberis <ed.barberis@gmail.com>
2025-07-10 14:59:24 -0400

    Updated support for MariaDB v11.4.

--- provisioners/scripts/ubuntu/install_ubuntu_mariadb_community_server_11.sh
+++ provisioners/scripts/ubuntu/install_ubuntu_mariadb_community_server_11.sh
@@ -1,6 +1,6 @@
-#!/bin/sh -eux
+#!/bin/bash -eux
 #---------------------------------------------------------------------------------------------------
-# Install MariaDB Community Server 11.4 by MariaDB on CentOS Linux 7.x.
+# Install MariaDB Community Server 11.4 by MariaDB on Ubuntu Linux.
 #
 # MariaDB Server is one of the most popular open source relational databases. It’s made by the
 # original developers of MySQL and guaranteed to stay open source. It is part of most cloud
@@ -28,22 +28,54 @@ set +x  # temporarily turn command display OFF.
 mariadb_server_root_password="${mariadb_server_root_password:-Password1234!}"   # [optional] root password (defaults to 'Password1234!').
 set -x  # turn command display back ON.
 
-# [OPTIONAL] devops home folder [w/ default].
-devops_home="${devops_home:-/opt/devops}"                                       # [optional] devops home (defaults to '/opt/devops').
+# validate ubuntu release version. -----------------------------------------------------------------
+# check for supported ubuntu release.
+ubuntu_release=$(lsb_release -rs)
 
-# create scripts directory (if needed). ------------------------------------------------------------
-mkdir -p ${devops_home}/provisioners/scripts/centos
-cd ${devops_home}/provisioners/scripts/centos
+if [ -n "$ubuntu_release" ]; then
+  case $ubuntu_release in
+      20.04|22.04|24.04|25.04)
+        ;;
+      *)
+        echo "Error: MongoDB NOT supported on Ubuntu release: '$(lsb_release -ds)'."
+        exit 1
+        ;;
+  esac
+fi
 
-# configure mariadb yum repository. ----------------------------------------------------------------
-rm -f mariadb_repo_setup
-wget --no-verbose https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
-chmod 755 ./mariadb_repo_setup
-./mariadb_repo_setup
+# update the apt repository package indexes. -------------------------------------------------------
+apt-get update
+
+# prepare the mariadb server package for installation. ---------------------------------------------
+# import the mariadb server repository key onto our ubuntu system.
+sudo apt-get install apt-transport-https curl
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -o /etc/apt/keyrings/mariadb-keyring.pgp 'https://mariadb.org/mariadb_release_signing_key.pgp'
+
+# get current date/time in utc format.
+# example: 2025-04-17 20:57 UTC
+utc_date_time=$(date -u +"%Y-%m-%d %H:%M UTC")
+
+# generate the mariadb server sources file.
+rm -f /etc/apt/sources.list.d/mariadb.sources
+
+cat <<EOF > /etc/apt/sources.list.d/mariadb.sources
+# MariaDB 11.4 repository list - created ${utc_date_time}
+# https://mariadb.org/download/
+X-Repolib-Name: MariaDB
+Types: deb
+# deb.mariadb.org is a dynamic mirror if your preferred mirror goes offline. See https://mariadb.org/mirrorbits/ for details.
+# URIs: https://deb.mariadb.org/11.4/ubuntu
+URIs: https://mirror.its.dal.ca/mariadb/repo/11.4/ubuntu
+Suites: $(lsb_release -cs)
+Components: main main/debug
+Signed-By: /etc/apt/keyrings/mariadb-keyring.pgp
+EOF
 
 # install mariadb server. --------------------------------------------------------------------------
 # install mariadb server binaries.
-yum -y install MariaDB-server
+apt-get update
+apt-get -y install mariadb-server
 
 # configure mariadb server. ------------------------------------------------------------------------
 # start the mariadb service and configure it to start at boot time.
